5f9383a4885c67a1eee895b27464a38da7f83b12
shivamagarwalhere <shivam2002tintin@gmail.com>
2024-04-06 01:03:25 +0530

    Login/Signup Auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import db from '@/lib/supabase/db';
 import { ThemeProvider } from "@/lib/providers/next-theme-provider";
+import { DM_Sans} from 'next/font/google';
+import { twMerge } from 'tailwind-merge';
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,10 +19,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  console.log(db);
+  //console.log(db);
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={twMerge('bg-background', inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
